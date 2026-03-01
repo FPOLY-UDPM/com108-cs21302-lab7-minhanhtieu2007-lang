2b7d02a63ea32b6d86ab99c428f786fccd5b3750
minhanhtieu2007-lang <minhanhtieu2007@gmai.com>
2026-03-01 11:16:09 +0700

    bai 2 3

--- bai2.c
+++ bai2.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: Tiêu Anh Minh
+ * MSSV:      PS48358
+ * Lớp:       CS21301
  *****************************************************************************/
 
 //  BÀI 2: XÂY DỰNG CHƯƠNG TRÌNH ĐĂNG NHẬP BẰNG USERNAME VÀ PASSWORD 
@@ -9,4 +9,25 @@
 //  Output: Đăng nhập thành công hay không thành công
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+    char username[50];
+    char password[50];
+
+    printf("Nhap username: ");
+    scanf("%s", username);
+
+    printf("Nhap password: ");
+    scanf("%s", password);
+
+    // So sánh username và password
+    if (strcmp(username, "admin") == 0 && strcmp(password, "123456") == 0) {
+        printf("Dang nhap thanh cong!");
+    } else {
+        printf("Dang nhap khong thanh cong!");
+    }
+
+    return 0;
+}
--- bai3.c
+++ bai3.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: Tiêu Anh Minh
+ * MSSV:      PS48358
+ * Lớp:       CS21301
  *****************************************************************************/
 
 //  BÀI 3: XÂY DỰNG CHƯƠNG TRÌNH SẮP XẾP CHUỖI THEO CHỮ CÁI 
@@ -9,4 +9,36 @@
 //  Output: Thứ tự các chuỗi đã được sắp xếp  
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+    char str[5][100];
+    char temp[100];
+
+    // Nhập 5 chuỗi
+    for (int i = 0; i < 5; i++) {
+        printf("Nhap chuoi %d: ", i + 1);
+        scanf("%s", str[i]);
+    }
+
+    // Sắp xếp (Bubble Sort)
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 5; j++) {
+            if (strcmp(str[i], str[j]) > 0) {
+                // Hoán đổi
+                strcpy(temp, str[i]);
+                strcpy(str[i], str[j]);
+                strcpy(str[j], temp);
+            }
+        }
+    }
+
+    // In kết quả
+    printf("\nChuoi sau khi sap xep:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%s\n", str[i]);
+    }
+
+    return 0;
+}
